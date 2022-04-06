37bfe55733ce5402190de63a243a386d2b4cfab0
AlPHA__830 <68626539+CoachAlpha@users.noreply.github.com>
2022-04-05 23:21:08 -0400

    website click tracker

---
+++ static/js/datatables-simple-demo.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+});
